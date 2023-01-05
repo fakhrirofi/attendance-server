f56e9c9ae3dcccd717117abeaef60c561dbb7e2b
fakhrirofi <fakhricrofi@gmail.com>
2023-01-06 00:12:25 +0700

    update models and test

--- app/models.py
+++ app/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.db.models.signals import m2m_changed
+from django.shortcuts import get_object_or_404
+from django.utils import timezone
+
 
 class Team(models.Model):
     name = models.CharField("team name", max_length=20, unique=True)
@@ -6,24 +10,93 @@ class Team(models.Model):
     def __str__(self) -> str:
         return f"<Team {self.name}>"
 
+
 class User(models.Model):
-    name = models.CharField("name", max_length=30, unique=True)
-    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    user_id = models.IntegerField("user id", primary_key=True, unique=True)
+    name = models.CharField("name", max_length=30)
+    team = models.ForeignKey(Team, blank=False, on_delete=models.CASCADE)
 
     def __str__(self) -> str:
         return f"<User {self.name}>"
 
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        user = None
+        for user in self.team.user_set.all():
+            if user.name == self.name:
+                user = user
+                break
+        for event in self.team.event_set.all():
+            Presence.objects.create(user=user, event=event)
+
+
 class Event(models.Model):
-    name = models.CharField("event name", max_length=30)
+    event_id = models.IntegerField("event id", primary_key=True, unique=True)
+    name = models.CharField("event name", max_length=30, unique=True)
+    participant = models.ManyToManyField(Team)
+    date = models.DateField("date", null=True)
 
     def __str__(self) -> str:
         return f"<Event {self.name}>"
 
+
+def participant_changed(sender, **kwargs):
+    event = kwargs["instance"]
+    action = kwargs["action"]
+    for id in kwargs['pk_set']:
+        for user in Team.objects.get(pk=id).user_set.all():
+            if action == "post_add":
+                Presence.objects.create(user=user, event=event)
+            elif action == "post_remove":
+                Presence.objects.filter(user=user, event=event).delete()
+
+m2m_changed.connect(participant_changed, sender=Event.participant.through)
+
+
 class Presence(models.Model):
-    user = models.ManyToManyField(User)
-    event = models.ManyToManyField(Event)
-    date = models.DateTimeField('date')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    attend = models.BooleanField("Attendance", default=False)
+    datetime = models.DateTimeField('datetime', null=True)
 
     def __str__(self) -> str:
         return f"<Presence {self.user.name} on {self.event.name}>"
 
+
+def attend(user_id: int, event_id: int):
+    user = get_object_or_404(User, user_id=user_id)
+    event = get_object_or_404(Event, event_id=event_id)
+    if user.team not in event.participant.all():
+        Event.objects.get_or_create(name=f"NOT INCLUDED {event.name}")
+        event = Event.objects.get(name=f"NOT INCLUDED {event.name}")
+        Presence.objects.create(user=user, event=event, attend=True, datetime=timezone.now())
+        return "not_included"
+
+    presence = Presence.objects.filter(user=user, event=event).first()
+    presence.attend = True
+    presence.datetime = timezone.now()
+    presence.save()
+    return "success"
+
+def get_events():
+    data = list()
+    for event in Event.objects.order_by('-date', '-event_id').all():
+        data.append({
+            'event_id'      : event.event_id,
+            'name'          : event.name,
+            'date'          : event.date,
+            'team_count'    : event.participant.count()
+        })
+    return data
+
+def get_event_participant(event_id):
+    data = list()
+    event = Event.objects.get(event_id=event_id)
+    for presence in Presence.objects.filter(event=event).all():
+        data.append({
+            'name'      : presence.user.name,
+            'team'      : presence.user.team.name,
+            'attend'    : presence.attend,
+            'datetime'  : presence.datetime
+        })
+    return data
--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,90 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import * 
+
+class ModelTest(TestCase):
+
+    def setUp(self):
+        Team.objects.create(name='synthesis')
+        User.objects.create(user_id=1, name='alex', team=Team.objects.first())
+        Event.objects.create(event_id=1, name='opening')
+        event = Event.objects.get(name='opening')
+        event.participant.add(Team.objects.get(name='synthesis'))
+
+    def test_team_name_unique(self):
+        def create_team():
+            team1 = Team(name='synthesis')
+            team1.save() # Error unique
+        self.assertRaises(Exception, create_team)
+
+    def test_user_must_have_team(self):
+        def create_user():
+            user1 = User(name='alex')
+            user1.save()
+        self.assertRaises(Exception, create_user)
+
+    def test_user_name_not_unique(self):
+        user1 = User(name='alex', team=Team.objects.first())
+        user1.save()
+
+    def test_event_name_unique(self):
+        def create_event():
+            event1 = Event(name='opening')
+            event1.save() # Error unique
+        self.assertRaises(Exception, create_event)
+
+    def test_attend_success(self):
+        self.assertEqual(attend(1, 1), 'success')
+
+    def test_attend_not_included(self):
+        Team.objects.create(name='second')
+        User.objects.create(name='alex', team=Team.objects.get(pk=2))
+        self.assertEqual(attend(2, 1), 'not_included')
+        self.assertEqual(f"NOT INCLUDED opening", Event.objects.get(pk=2).name)
+        self.assertIn(
+            User.objects.get(pk=2),
+            [presence.user for presence in Presence.objects.filter(event=Event.objects.get(pk=2)).all()]
+        )
+        # another user
+        User.objects.create(name='alex', team=Team.objects.get(pk=2))
+        self.assertEqual(attend(3, 1), 'not_included')
+        self.assertEqual(len(Event.objects.all()), 2)
+        self.assertEqual(len(Presence.objects.filter(event=Event.objects.get(pk=2)).all()), 2)
+
+    def test_new_user_team_already_on_event(self):
+        User.objects.create(name="alex 2", team=Team.objects.get(pk=1))
+        self.assertEqual(len(Presence.objects.all()), 2)
+
+    def test_add_and_remove_team_on_event(self):
+        Team.objects.create(name='team 2')
+        team = Team.objects.get(pk=2)
+        User.objects.create(name='user 2', team=team)
+        event = Event.objects.get(event_id=1)
+
+        event.participant.add(team)
+        self.assertEqual(len(Presence.objects.all()), 2)
+
+        event.participant.remove(team)
+        self.assertEqual(len(Presence.objects.all()), 1)
+
+    def test_get_event(self):
+        self.assertEqual(
+            get_events(),
+            [{
+                'event_id'  : 1,
+                'name'      : 'opening',
+                'date'      : None,
+                'team_count': 1
+            }]
+        )
+
+    def test_get_event_participant(self):
+        self.assertEqual(
+            get_event_participant(1),
+            [{
+            'name'      : 'alex',
+            'team'      : 'synthesis',
+            'attend'    : False,
+            'datetime'  : None
+            }]
+        )
