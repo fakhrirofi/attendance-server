252651515e78b2f30a8358ec48e522049b39e3c5
fakhrirofi <fakhricrofi@gmail.com>
2023-04-25 23:59:59 +0700

    add website page

--- seminar/views.py
+++ seminar/views.py
@@ -2,4 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Hello, World")
+    return render(request, 'seminar/index.html')
+
+def registration(request):
+    return render(request, 'seminar/registration.html')
