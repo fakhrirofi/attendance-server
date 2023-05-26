b7c18e3e9253e2c2d70347686fc2cbba582e8136
fakhrirofi <fakhricrofi@gmail.com>
2023-05-27 03:05:36 +0700

    add attendance count to event

--- event/admin/event.py
+++ event/admin/event.py
@@ -4,12 +4,15 @@ from . import admin, Event, ModelAdmin
 class EventAdmin(ModelAdmin):
     list_display_links = ['name']
     list_display = [
-        'name', 'datetime', 'place', 'is_free', 'is_open', 'registered', 
+        'name', 'datetime', 'place', 'is_free', 'is_open', 'registered', 'attendance', 
     ]
     ordering = ['pk']
 
     def registered(self, obj):
         return obj.presence_set.count()
+
+    def attendance(self, obj):
+        return obj.presence_set.filter(attendance=True).count()
 
 
 admin.site.register(Event, EventAdmin)
